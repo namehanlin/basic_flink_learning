35ca1f74cef3c9123f702142bbdc05c6770f925a
name_hanlin@163.com <lover940614>
2020-01-14 17:27:41 +0800

    123

---
+++ src/main/java/com/hanlin/flink/basic/core/localFile.java
@@ -0,0 +1,21 @@
+package com.hanlin.flink.basic.core;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * Copyright (C), 2020
+ * Author: 韩林
+ * Date: 2020/1/14 17:18
+ * FileName: localFile
+ * Description: 本地文件操作
+ */
+public class localFile {
+
+    public static void main(String[] args) {
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+
+    }
+
+}
---
+++ src/main/java/com/hanlin/flink/basic/core/socketWindow.java
@@ -0,0 +1,20 @@
+package com.hanlin.flink.basic.core;
+
+/**
+ * Copyright (C), 2020
+ * Author: 韩林
+ * Date: 2020/1/14 17:15
+ * FileName: core
+ * Description: flink基础
+ */
+public class socketWindow {
+
+    public static void main(String[] args) {
+
+        // 创建上下文
+
+
+
+    }
+
+}
